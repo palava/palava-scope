5d0530411259a1739432abe313cd5d7d54d2f136
Willi Schoenborn <schoenborn@cosmocode.de>
2011-01-22 13:33:27 +0100

    added missing @param description

--- src/main/java/de/cosmocode/palava/scope/NoopKeyEncoder.java
+++ src/main/java/de/cosmocode/palava/scope/NoopKeyEncoder.java
@@ -7,6 +7,7 @@ import com.google.inject.Key;
  * A noop implementation used by {@link ScopingProvider}.
  * 
  * @author Willi Schoenborn
+ * @param <T> generic key type
  */
 final class NoopKeyEncoder<T> implements Function<Key<T>, Object> {
 
